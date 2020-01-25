00c8d459edc3fe0c2c5ab262b6b594e0bb238941
Adam Ayd <adam.ayd@gmail.com>
2020-01-25 15:52:01 -0500

    converted product-detail to JSX

--- web/pages/product-detail.js
+++ web/pages/product-detail.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Header from '../components/header';
+import Breadcrumbs from '../components/breadcrumbs';
+import Detail from '../components/detail';
+import RelatedProducts from '../components/related_products';
 import Footer from '../components/footer';
 
 const HomePage = () => {
     return (
         <>
             <Header/>
+            <Breadcrumbs/>
+            <Detail/>
+            <RelatedProducts/>
             <Footer />
         </>
     )
