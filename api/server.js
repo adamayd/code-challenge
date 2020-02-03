414668b70e7e5f424f08f10288aded714319cc2b
Adam Ayd <adam.ayd@gmail.com>
2020-02-02 22:05:46 -0500

    added eslint to api and resolved issues

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const request = require("request");
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
 const cors = require('cors');
 
 const app = express();
@@ -10,33 +10,31 @@ const apiUrl = 'https://next.json-generator.com/api/json/get/EkzBIUWNL';
 app.use(bodyParser.json());
 app.use(cors());
 
+// API function
+const apiCall = (url) => new Promise((resolve, reject) => {
+  request(url, { json: true }, (err, res, body) => {
+    if (err) reject(err);
+    resolve(body);
+  });
+});
+
 // GetMany
-app.get("/api/getmany", (req, res) => {
-    console.log("GET Many");
-    apiCall(apiUrl)
-        .then(response => res.json(response))
-        .catch(error => res.status(500).send(error));
+app.get('/api/getmany', (req, res) => {
+  console.log('GET Many');
+  apiCall(apiUrl)
+    .then((response) => res.json(response))
+    .catch((error) => res.status(500).send(error));
 });
 
 // GetSingle
-app.get("/api/getsingle/:id", (req, res) => {
-    console.log(`GET Single ${req.params.id}`);
-    apiCall(apiUrl)
-        .then(response => res.json(response.find(product => product.guid === req.params.id)))
-        .catch(error => res.status(500).send(error));
+app.get('/api/getsingle/:id', (req, res) => {
+  console.log(`GET Single ${req.params.id}`);
+  apiCall(apiUrl)
+    .then((response) => res.json(response.find((product) => product.guid === req.params.id)))
+    .catch((error) => res.status(500).send(error));
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
-
-// API function
-const apiCall = (url) => {
-    return new Promise((resolve, reject) => {
-        request(url, {json: true}, (err, res, body) => {
-            if (err) reject(err)
-            resolve(body)
-        });
-    });
-};
